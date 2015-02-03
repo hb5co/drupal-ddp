e6099d2b1a32df7677c2279763eb6a890297c40a
Bayo Fodeke <bayof@knowclassic.com>
2015-02-03 12:30:05 -0500

    Large update
    - Added new 'drupalDdpTaxonomies' collection
    - Handle insert, update, and delete of Taxonomies
    - Handle deleting of nodes

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 drupalDdpNodes = new Mongo.Collection('drupal_ddp_nodes');
+drupalDdpTaxonomies = new Mongo.Collection('drupal_ddp_taxonomies');
 
 if (Meteor.isServer) {
   Meteor.methods({
     // Accept node inserts and updates from Drupal.
     DrupalSaveNode: function (data) {
-      if (data.node.is_new) {
-        // Add new nodes.
-        drupalDdpNodes.insert(data);
+      console.log(data);
+      // Handle Nodes
+      if(data.content.ddp_type == 'node'){
+        if (data.content.is_new) {
+          // Add new posts.
+          drupalDdpNodes.insert(data);
+        }
+        else if(data.content.delete_content){
+          // Delete existing posts.
+          drupalDdpNodes.remove({"content.nid": data.content.nid},{$set:{content:data.content}});
+        }
+        else {
+          // Update existing posts.
+          drupalDdpNodes.update({"content.nid": data.content.nid},{$set:{content:data.content}});
+        }
       }
-      else {
-        // Update existing nodes.
-        drupalDdpNodes.update({"node.nid": data.node.nid},{$set:{node:data.node}});
+
+      // Handle Taxonomies
+      if(data.content.ddp_type == 'taxonomy'){
+        if (data.content.is_new) {
+          drupalDdpTaxonomies.insert(data);
+        }
+        else if(data.content.delete_content){
+          // Delete existing taxonomies.
+          drupalDdpTaxonomies.remove({"content.tid": data.content.tid},{$set:{content:data.content}});
+        }
+        else {
+          // Update existing taxonomies.
+          drupalDdpTaxonomies.update({"content.tid": data.content.tid},{$set:{content:data.content}});
+        }
       }
     }
   });
