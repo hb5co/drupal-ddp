758a907abef56af3e32e0cebab0b6d15aa4e30a4
Bayo Fodeke <bfodeke@gmail.com>
2015-03-19 13:32:16 -0400

    added new methods
    Added 'base64Encode' to handle the encoding for restws auth.
    Added 'getDrupalDdpToken' to get auth token from Drupal.
    Added 'updateNodeInDrupal' to handle sending data back to drupal.

--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,7 @@
 Meteor.methods({
+  base64Encode: function (unencoded) {
+    return new Buffer(unencoded || '').toString('base64');
+  },
   // Accept node inserts and updates from Drupal.
   DrupalSaveNode: function (data) {
     console.log(data);
@@ -54,16 +57,115 @@ Meteor.methods({
         });
 
         // Set account 'verified' to true
-        Meteor.users.update({"profile.uid": data.content.uid}, {$set: {"emails.0.verified" :true}});
+        Meteor.users.update({"profile.uid" : data.content.uid}, {$set: {"emails.0.verified" : true}});
+        Meteor.users.update({"profile.uid" : data.content.uid}, {$set: {"services.password.bcrypt" : data.content.pass}});
       }
       else if(data.content.delete_content){
-        // Delete existing taxonomies.
-        drupalDdpUsers.remove({"content.tid": data.content.tid});
+        // Delete existing user.
+        user_id = Meteor.users.findOne({"profile.uid" : data.content.uid})._id;
+        Meteor.users.remove(user_id);
       }
       else {
-        // Update existing taxonomies.
-        drupalDdpUsers.update({"content.tid": data.content.tid},{$set:{content:data.content}});
+        // Update existing user.
+        // update profile
+        // update name & password
+        // update username
+        // update email
+        // update roles
+        // Meteor.users.update({"profile.uid" : data.content.uid});
+
       }
     }
-  }
+  },
+  getDrupalDdpToken: function() {
+    var drupalDdpUrl = Meteor.settings.drupal_ddp.ddp_url;
+    var options = {
+      url: drupalDdpUrl + "/restws/session/token",
+      username : Meteor.settings.drupal_ddp.restws_user,
+      password : Meteor.settings.drupal_ddp.restws_pass,
+    };
+
+    var auth = 'Basic ' + Meteor.call('base64Encode', options.username + ':' + options.password);
+
+    try {
+      var result = HTTP.post(options.url, {
+        headers: {
+          Authorization: auth
+        }
+      });
+
+      tokenResponse = {
+        token: result.content,
+        cookie: result.headers['set-cookie'][0],
+      }
+
+      return tokenResponse;
+    } catch (e) {
+      return false;
+    }
+  },
+  updateNodeInDrupal: function(node) {
+    tokenCookie = Meteor.call('getDrupalDdpToken');
+
+    // Get node body out of array
+    node.content.body =  node.content.body.und[0];
+
+    node = node.content;
+
+    // Remove items in the node that aren't supported for writing
+    // via restws in Drupal.
+    cleanUpNode = [
+      'is_new',
+      'vid',
+      'uid',
+      'ddp_type',
+      'comment',
+      'changed',
+      'tnid',
+      'translate',
+      'rdf_mapping',
+      'revision_timestamp',
+      'revision_uid',
+      'cid',
+      'last_comment_timestamp',
+      'last_comment_name',
+      'last_comment_uid',
+      'comment_count',
+      'name',
+      'picture',
+      'data'
+    ];
+
+    $.each(cleanUpNode, function(){
+      node.remove(this);
+    });
+
+    if (tokenCookie) {
+      try {
+        baseUrl = Meteor.settings.drupal_ddp.ddp_url;
+        endpoint = baseUrl + '/node/' + node.nid; 
+
+        var result = HTTP.put(
+          endpoint, 
+          {
+            headers: {
+              'Content-type': 'application/json',
+              'X-CSRF-Token': tokenCookie.token,
+              'Accept': 'application/json',
+              'Cookie': tokenCookie.cookie,
+            },
+            params: {
+              name: 'restws_login',
+              pass: 'password',
+              form_id: 'user_login_form'
+            },
+            data: node
+          }
+        );
+        return result;
+      } catch (e) {
+        return false;
+      }
+    }
+  },
 });
