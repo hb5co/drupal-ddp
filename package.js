c613e63ae42c72b3373dd51a474ffee3799616c0
Mark Shropshire <mdshrops@shropnet.net>
2015-04-24 16:08:24 -0400

    Fixed error: error: File not found: server.js

--- package.js
+++ package.js
@@ -37,6 +37,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.addFiles(['server.js'], 'server');
+  api.addFiles('server');
   api.addFiles('tests.js');
 });
