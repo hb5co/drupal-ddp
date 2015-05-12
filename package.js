3e7d34dc0bd9bc655d0fe00812e661d1b6f7ba52
Mark Shropshire <mdshrops@shropnet.net>
2015-05-12 07:10:49 -0400

    Updated version to 0.0.6

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'hb5:drupal-ddp',
   summary: 'Drupal and Meteor integration over DDP',
   git: 'https://github.com/hb5co/drupal-ddp',
-  version: '0.0.5'
+  version: '0.0.6'
 });
 
 Package.onUse(function(api) {
