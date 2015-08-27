58efa528531b05b40935c0c1555e55ca2d00bc5d
Bayo Fodeke <bfodeke@gmail.com>
2015-08-27 11:03:34 -0400

    Revved version number

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'hb5:drupal-ddp',
   summary: 'Drupal and Meteor integration over DDP',
   git: 'https://github.com/hb5co/drupal-ddp',
-  version: '0.1.0'
+  version: '0.1.1'
 });
 
 Package.onUse(function(api) {
